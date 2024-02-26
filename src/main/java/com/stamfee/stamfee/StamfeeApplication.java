9c95f119b448151be27c26e1e3af738c10713407
Coalman96 <mayuaa001@gmail.com>
2024-02-26 15:12:15 +0900

    Init : Init Project
    프로젝트 생성 및 의존성 주입

---
+++ src/main/java/com/stamfee/stamfee/StamfeeApplication.java
@@ -0,0 +1,13 @@
+package com.stamfee.stamfee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StamfeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StamfeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stamfee/stamfee/StamfeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stamfee.stamfee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StamfeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
